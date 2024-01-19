6f617251bd8f7e3f9a8cf189e6fbe2913d7b8104
about-suntao <1550208137@qq.com>
2024-01-19 16:56:59 +0800

    fix 国际班级介绍模块修改

--- src/app/home/introduction/introduction.tsx
+++ src/app/home/introduction/introduction.tsx
@@ -15,15 +15,17 @@ function Introduction() {
       id: 1,
       class: 'Class 1',
       img: classImg1,
-      title: '北美精英班',
-      introduce: '学生以美国前30，加拿大前3大学为升学目标，注重综合素质的全面提升与培养。',
+      title: 'AP北美精英班',
+      cultivate: '学生以美国前30，加拿大前3大学为升学目标，注重综合素质的全面提升与培养。<br/> 高一阶段，重点打好语言考试应试基础与理科类AP课程学习； <br/> 高二阶段，做好SAT美国高考应试突破与综合类AP课程学习；<br/>  高三阶段，美国升学方向的同学在升学指导老师的协助下开始完成美国大学申请文书的撰写与申请材料提交，加拿大升学方向的学生可选择高三赴加拿大姐妹校完成加拿大公立体系课程，修完并成绩合格后可直升加拿大排名第一的多伦多大学',
+      target: '50%学生升入美国前30大学 <br/> 100%学生升入美国前40及加拿大前3大学',
     },
     {
       id: 2,
       class: 'Class 2',
       img: classImg2,
-      title: '国际融合班',
-      introduce: '云顶精英国际高中在深圳首推的融合教学体系，结合各大体系的优势。学生以美国前20、英国G5、加拿大前3、香港前3、新加坡前2大学及多国联申为升学目标。',
+      title: 'APAL国际融合班',
+      cultivate: '学生以美国前20，英国G5，香港前3，新加坡前2大学及多国联申为升学目标。<br/>  在高一时期，以A-Level课程系统的知识点来夯实基础学科； <br/> 高二阶段根据学生的学习情况与升学目标，有的放矢地选择走A-Level为主，或是AP与A-Level双保险的升学路径。更有效地发挥了AP与A-Level各自优势。',
+      target: '50%学生升入英国前5，香港前3，新加坡前2大学 <br/> 100%学生升入世界前80大学',
     }
   ]
 
@@ -47,7 +49,10 @@ function Introduction() {
                     </div>
                     <div className={styles.card_c}>
                       <h2>{item.title}</h2>
-                      <p> {item.introduce}</p>
+                      <h3>阶段化培养：</h3>
+                      <p dangerouslySetInnerHTML={{ __html: item.cultivate }}></p>
+                      <h3>保障升学目标：</h3>
+                      <p dangerouslySetInnerHTML={{ __html: item.target }}></p>
                     </div>
                   </li>
                 )
